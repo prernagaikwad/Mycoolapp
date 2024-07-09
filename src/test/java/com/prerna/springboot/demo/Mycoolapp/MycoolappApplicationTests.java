62706709bcda4c08507ee921bee5d5745900602f
Prerna Gaikwad <prernagaikwad487@gmail.com>
2024-07-09 19:26:41 +0530

    Initial commit

---
+++ src/test/java/com/prerna/springboot/demo/Mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prerna.springboot.demo.Mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
